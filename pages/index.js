236fc50f15ca15d2ce4af7a52d744308728464b7
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2023-09-12 17:33:24 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Luciana sua linda, eu te amo. Se você me ama, por que roubou minha bicicleta? Heim?</h1>
+  return (
+    <h1>
+      Luciana sua linda, eu te amo. Se você me ama, por que roubou minha
+      bicicleta? Heim?
+    </h1>
+  );
 }
 
 export default Home;
