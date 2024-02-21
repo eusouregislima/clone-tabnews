dadb9f125bdf48f4232043fcbcdf7731b4910179
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-02-21 13:51:45 +0000

    implementation tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,13 @@
-// essa pasta criou uma rota pública /api/status
+// essa pasta criou uma rota pública /api/v1/status
 
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
